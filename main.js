3084f4c16d1b623b6f40cd7526c6e8ed8c06a43a
Fredi Karu <fredikaru@gmail.com>
2022-08-10 03:02:09 +0300

    Created functions for calculator operators

--- main.js
+++ main.js
@@ -5,11 +5,22 @@ let operator = "";
 const currentDisplayNumber = document.querySelector(".currentNumber");
 const previousDisplayNumber = document.querySelector(".previousNumber");
 
+window.addEventListener('keydown', handleKeyPress);
+
 const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    if(currentNum != "" && previousNum != "") {
+        calculate();
+    }
+});
 
 const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", ()=> {
+    addDecimal();
+})
 
 const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearCalculator);
 
 const numberButtons = document.querySelectorAll(".number");
 
@@ -22,5 +33,115 @@ numberButtons.forEach(btn => {
 });
 
 function handleNumber(number) {
-    console.log(number);
+     if(previousNum !== "" && currentNum !=="" && operator ===""){
+         previousNum = "";
+         currentDisplayNumber.textContent = currentNum;
+     }
+    if(currentNum.length <= 10) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+
+    }
+}
+
+operators.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+function handleOperator(op){
+
+    if(previousNum === ""){
+        previousNum = currentNum;
+        operatorCheck(op);
+    } else if (currentNum === ""){
+        operatorCheck(op);
+    } else {
+        calculate();
+        operator = op;
+        currentDisplayNumber.textContent = "0";
+        previousDisplayNumber.textContent = previousNum + " " + operator;
+    }    
+}
+
+function operatorCheck(text) {
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentDisplayNumber.textContent = "0";
+    currentNum = "";
+}
+
+function calculate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if (operator === "+") {
+        previousNum += currentNum;
+    } else if (operator === "-") {
+        previousNum -= currentNum;
+    } else if (operator === "x") {
+        previousNum *= currentNum;
+    } else if (operator === "/") {
+        if (currentNum <= 0){
+            previousNum ="Error"
+            displayResults();
+            return;
+        }
+        previousNum /= currentNum;
+    }
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResults();
+}
+
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
+}
+function displayResults() {
+    
+    if(previousNum.length <= 11) {
+        currentDisplayNumber.textContent = previousNum;
+    } else {
+        currentDisplayNumber.textContent = previousNum.slice(0,11) + "...";
+    }
+    previousDisplayNumber.textContent = "";
+    operator = "";
+    currentNum = "";
+}
+
+function clearCalculator() {
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+    currentDisplayNumber.textContent = "0";
+    previousDisplayNumber.textContent = "";
+}
+
+function addDecimal() {
+    if (!currentNum.includes('.')){
+        currentNum += ".";
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+function handleKeyPress(e) {
+    e.preventDefault()
+    if (e.key >= 0 && e.key <= 9){
+        handleNumber(e.key)
+    }
+    if (e.key === "Enter" || 
+        (e.key === "=" && currentNum!="" && previousNum != "")
+        ) {
+        compute();
+    }
+    if (e.key ==="+" || e.key ==="-" || e.key ==="/"){
+        handleOperator(e.key)
+    }
+    if (e.key ==="*") {
+        handleOperator("x");
+    }
+    if (e.key === ".") {
+        addDecimal();
+    }
 }
